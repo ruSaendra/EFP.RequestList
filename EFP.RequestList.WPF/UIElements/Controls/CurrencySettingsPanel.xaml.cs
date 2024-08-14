61d02db96e81e87a187fba9cc7dde6abc6f5b7e7
saendra.ru <saendra.ru@gmail.com>
2024-08-14 22:18:15 +0300

    Add user controls.

---
+++ EFP.RequestList.WPF/UIElements/Controls/CurrencySettingsPanel.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EFP.RequestList.WPF.UIElements.Controls
+{
+    /// <summary>
+    /// Interaction logic for CurrencySettingsPanel.xaml
+    /// </summary>
+    public partial class CurrencySettingsPanel : UserControl
+    {
+        public CurrencySettingsPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void addBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void rmBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
