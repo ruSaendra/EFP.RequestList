f02b3946c4d8ece6640aa050a229d639eb04c5ee
saendra.ru <saendra.ru@gmail.com>
2024-08-15 20:07:39 +0300

    Addition to previous commit.

---
+++ EFP.RequestList.Libraries/DataStructures/DataBase/CurrencyRate.cs
@@ -0,0 +1,16 @@
+﻿namespace EFP.RequestList.Libraries.DataStructures.DataBase
+{
+    public class CurrencyRate
+    {
+        public uint Id { get; set; }
+
+        public uint CurrencyId { get; set; }
+        public Currency Currency { get; set; }
+
+        public double Rate { get; set; }
+
+        public DateTime DateTimeStart { get; set; }
+
+        public DateTime DateTimeEnd { get; set; }
+    }
+}
